a651da6b7b02cdebee20097a0b210fb82e916991
Sébastien Le Callonnec <sebastien@weblogism.com>
2020-01-30 06:13:26 +0000

    Rewrote dijkstra logic to fix problem.
    Cf. algo with priority queue:
    https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Using_a_priority_queue

--- src/graph_sandbox/dijkstra.clj
+++ src/graph_sandbox/dijkstra.clj
@@ -1,6 +1,8 @@
 (ns graph-sandbox.dijkstra
   (:require [clojure.data.priority-map :refer [priority-map]]))
 
+(def infinity (Double/POSITIVE_INFINITY))
+
 (def graph {:A [{:B 2} {:C 4}]
             :B [{:D 2} {:F 3}]
             :C [{:E 2} {:F 3}]
@@ -11,8 +13,8 @@
             :F [{:J 1} {:G 4}]
             :G [{:J 1}]})
 
-(defn visited? [node visited]
-  (contains? (set visited) node))
+;; (defn visited? [node visited]
+;;   (contains? (set visited) node))
 
 (defn neighbours [node graph]
   (graph node))
@@ -22,37 +24,63 @@
        (some #(when (contains? % to) %))
        to))
 
-(defn next-cost [cost-so-far current next]
-  (let [c (cost-so-far current)]
-    (sort-by (comp second first)
-             (map #(reduce-kv (fn [map key val] (assoc map key (+ c val))) {} %) next))))
+;; (defn next-cost [cost-so-far current next]
+;;   (let [c (cost-so-far current)]
+;;     (sort-by (comp second first)
+;;              (map #(reduce-kv (fn [map key val] (assoc map key (+ c val))) {} %) next))))
 
-(defn merge-min-map [m other]
-  (merge-with #(if (< %1 %2) %1 %2) m other))
+;; (defn merge-min-map [m other]
+;;   (merge-with #(if (< %1 %2) %1 %2) m other))
 
-(defn merge-min-cost-map [came-from from cost new-cost]
-  (if (empty? new-cost)
-    came-from
-    (let [[k v] (first new-cost)]
-      (if (or (not (contains? cost k))
-              (< v (cost k)))
-        (merge-min-cost-map (assoc came-from k from) from cost (dissoc new-cost k))
-        (merge-min-cost-map came-from from cost (dissoc new-cost k))))))
+;; (defn merge-min-cost-map [came-from from cost new-cost]
+;;   (if (empty? new-cost)
+;;     came-from
+;;     (let [[k v] (first new-cost)]
+;;       (if (or (not (contains? cost k))
+;;               (< v (cost k)))
+;;         (merge-min-cost-map (assoc came-from k from) from cost (dissoc new-cost k))
+;;         (merge-min-cost-map came-from from cost (dissoc new-cost k))))))
 
-(defn dijkstra-search [graph start end]
-  (loop [frontier (priority-map start 0)
-         came-from {start nil}
-         cost-so-far {start 0}]
-    (let [current (first (peek frontier))
-          next (neighbours current graph)
-          costs (reduce merge {} (next-cost cost-so-far current next))]
-      (println came-from cost-so-far)
-      (if (= current end)
-        {:from came-from :cost cost-so-far}
-        (recur (merge-with #(if (< %1 %2) %1 %2) (pop frontier) costs)
-               (merge-min-cost-map came-from current cost-so-far costs)
-               ;; (reduce #(assoc %1 %2 current) came-from (flatten (map keys next)))
-               (merge-with #(if (< %1 %2) %1 %2) frontier costs))))))
+;; (defn dijkstra-search [graph start end]
+;;   (loop [frontier (priority-map start 0)
+;;          came-from {start nil}
+;;          cost-so-far {start 0}]
+;;     (let [current (first (peek frontier))
+;;           next (neighbours current graph)
+;;           costs (reduce merge {} (next-cost cost-so-far current next))]
+;;       (println came-from cost-so-far)
+;;       (if (empty? frontier)
+;;         {:from came-from :cost cost-so-far}
+;;         (recur (merge-with #(if (< %1 %2) %1 %2) (pop frontier) costs)
+;;                (merge-min-cost-map came-from current cost-so-far costs)
+;;                ;; (reduce #(assoc %1 %2 current) came-from (flatten (map keys next)))
+;;                (merge-with #(if (< %1 %2) %1 %2) frontier costs))))))
+
+
+(defn visit-neighbours [graph u neighbours distance previous queue]
+  (loop [n neighbours
+         dist distance
+         prev previous
+         q queue]
+    (if (empty? n)
+      {:distance dist :previous prev :queue q}
+      (let [[v _] (first (first n))
+            alt (+ (dist u) (cost graph u v))]
+        (if (< alt (or (dist v) infinity))
+          (recur (rest n) (assoc dist v alt) (assoc prev v u) (assoc q v alt))
+          (recur (rest n) dist prev q))))))
+
+(defn dijkstra-search [graph start _end]
+  (loop [queue (priority-map start 0)
+         distance {start 0}
+         previous {start nil}]
+    (if (empty? queue)
+      {:distance distance :from previous}
+    (let [current (first (peek queue))
+          neighbours (neighbours current graph)
+          next (visit-neighbours graph current neighbours distance previous (pop queue))]
+      (recur (next :queue) (next :distance) (next :previous))))))
+
 
 (defn path [graph start end f]
   (let [res (:from (f graph start end))]
